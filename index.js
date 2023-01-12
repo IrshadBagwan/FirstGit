e8f853b1247e77ed0632a9e01283beb3ba65f93b
IrshadBagwan <irshadbagwan706@gmail.com>
2023-01-12 12:00:39 +0530

    I changed the code

---
+++ index.js
@@ -0,0 +1,15 @@
+const headertitle = document.getElementById('header-title');
+console.log(headertitle);
+// headertitle.textContent = "hello";
+// headertitle.innerText = "goodbye";
+headertitle.style.borderBottom = "solid 3px #000";
+
+
+const additem = document.getElementsByClassName('title');
+console.log(additem);
+console.log(additem[0])
+additem[0].style.fontWeight='bold';
+additem[0].style.color='green';
+
+
+
