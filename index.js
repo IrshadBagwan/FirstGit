4e5a4873199cd01baa28bdb28f27210b4a4e645c
IrshadBagwan <irshadbagwan706@gmail.com>
2023-01-14 12:56:35 +0530

    all elements

--- index.js
+++ index.js
@@ -47,11 +47,94 @@ item3.style.display="none";
 
 const odd=document.querySelectorAll('li:nth-child(odd)');
 console.log(odd)
+
 for(let i=0;i<odd.length;i++){
    odd[i].style.backgroundColor="green";
 }
+
+const itemlist = document.querySelector('#items');
+// ParentNode
+// console.log(itemlist.parentNode);
+// itemlist.parentNode.style.backgroundColor="#f4f4f4";
+// console.log(itemlist.parentNode.parentNode)
    
- 
+ // Parentelement
+console.log(itemlist.parentElement);
+itemlist.parentElement.style.backgroundColor="#f4f4f4";
+console.log(itemlist.parentElement.parentElement)
+
+
+//ChildNodes
+console.log(itemlist.childNodes)
+
+console.log(itemlist.children);
+console.log(itemlist.children[1]);
+itemlist.children[1].style.backgroundColor="red";
+
+//FirstChild
+console.log(itemlist.firstChild);
+
+//FirstElementchild
+console.log(itemlist.firstElementChild);
+itemlist.firstElementChild.textContent="Hello 1";
+
+//lastChild
+console.log(itemlist.lastChild);
+
+//lastElementchild
+console.log(itemlist.lastElementChild);
+itemlist.lastElementChild.textContent="Hello 4";
+
+//nextSibling
+console.log(itemlist.nextSibling)
+
+//nextelementsibling
+console.log(itemlist.nextElementSibling)
+
+//previoussibling
+console.log(itemlist.previousSibling)
+
+//previouselementsibling
+console.log(itemlist.previousElementSibling)
+
+//CreateElement
+
+
+//create a div
+const newDiv = document.createElement('div');
+//Add class
+newDiv.className="hello";
+
+//Add id
+newDiv.id="hello1";
+
+//Add attr
+newDiv.setAttribute('title','Hello Div');
+
+//create text node
+const newDivText = document.createTextNode("hello world");
+
+//Add text to div
+newDiv.appendChild(newDivText);
+
+const container = document.querySelector('header .container');
+const h1 = document.querySelector('header h1');
 
 
 
+console.log(newDiv);
+newDiv.style.fontSize='30px';
+
+container.insertBefore(newDiv,h1)
+
+// listtag
+const newli = document.createElement('li');
+newli.className='list-group-item';
+
+newnodetext = document.createTextNode('Hello');
+newli.appendChild(newnodetext);
+
+const cont = document.querySelector('#items');
+const first  = document.querySelector('#first');
+
+cont.insertBefore(newli,first);
