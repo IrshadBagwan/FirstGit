72d958eb7d249e1c54100768cc5b1d17acc88c0c
IrshadBagwan <irshadbagwan706@gmail.com>
2023-01-14 14:11:20 +0530

    delete functionality

---
+++ main.js
@@ -0,0 +1,63 @@
+const form = document.getElementById('addForm');
+const itemlist = document.getElementById('items');
+const filter = document.getElementById('filter');
+
+form.addEventListener('submit',addItem);
+
+itemlist.addEventListener('click', removeItem);
+
+
+filter.addEventListener('keyup', filterItems);
+
+
+
+function addItem(e){
+    e.preventDefault();
+     // get input value
+    const newItem = document.getElementById('item').value;
+
+    // create li element
+    const li = document.createElement('li');
+    // Add class
+    li.className = 'list-group-item';
+     // Add text node with Input value
+
+     li.appendChild(document.createTextNode(newItem));
+
+     // create delete button element
+     const deleteBtn = document.createElement('button');
+     
+     // Add classes to delete button
+     deleteBtn.className='btn btn-danger btn-sm float-right delete';
+     //Append text Node
+     deleteBtn.appendChild(document.createTextNode('X'));
+
+      //Append button to li
+     li.appendChild(deleteBtn);
+
+     // Append li to list
+     itemlist.appendChild(li);
+
+     const edit = document.createElement('button');
+     // edit Button
+     edit.className='btn btn-danger btn-sm float-right delete';
+     //Append text Node
+     edit.appendChild(document.createTextNode('Edit'));
+
+      //Append button to li
+     li.appendChild(edit);
+
+     // Append li to list
+     itemlist.appendChild(li);
+}
+
+function removeItem(e){
+   if(e.target.classList.contains('delete')){
+      if(confirm('Are You Sure?')){
+          const li = e.target.parentElement;
+          
+          itemlist.removeChild(li);
+      }
+   }
+}
+
